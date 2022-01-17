e691596bdd403e8940f92a130f8911d3e1acf3fe
Himanshu Shukla <hshukla2611@gmail.com>
2022-01-17 14:25:57 +0530

    leetcode question 34 complete

--- Binary Search/src/com/himanshu/Leet34.java
+++ Binary Search/src/com/himanshu/Leet34.java
@@ -4,48 +4,45 @@ public class Leet34 {
     public static void main(String[] args) {
         int[] arr = {5, 7, 7, 7, 9, 21, 33};
         int[] result = searchRange(arr, 7);
-        System.out.println(result);
+        System.out.println("Done");
     }
 
     //  Find First and Last Position of Element in Sorted Array
     // Binary Search then get first element while going to the left and last element while going to right
     static int[] searchRange(int[] nums, int target) {
-        // if the target is greater than the last element in the array return -1
-        if(target > nums[nums.length-1]) {
-            return new int[]{-1, -1};
+        int[] ans = { -1, -1};
+
+        // check for fist occurrence if target first
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
         }
+        return ans;
+    }
 
+    // returns index value of target
+    static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
         int start = 0;
-        int end = nums.length - 1;
+        int end = nums.length-1;
 
-        int firstOccurrence=-1, lastOccurrence=-1;
-
-        while (start<=end) {
-            // find middle element
-            // start+end/2 might exceed integer value limit. so below code is more efficient
+        while (start <= end) {
             int mid = start + (end-start)/2;
 
             if (target < nums[mid]) {
-                if (mid-1 == target) {
-                    end = mid - 2;
-                    lastOccurrence = end;
-                } else {
-                    end = mid-1;
-                    lastOccurrence = end;
-                }
+                end = mid - 1;
             } else if (target > nums[mid]) {
-                if (mid+1 == target) {
-                    start = mid+2;
-                    firstOccurrence = end;
-                }
-                else {
-                    start = mid+1;
-                    firstOccurrence = end;
-                }
+                start = mid + 1;
             } else {
-                return new int[] {nums[firstOccurrence], nums[lastOccurrence]};
+                // potential ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
-        return new int[] {-1, -1};
+        return ans;
     }
 }
